a7aeb83a98262f817ec04c82781dee30776fb74b
17wh1a0578 <61539991+17wh1a0578@users.noreply.github.com>
2020-03-03 22:21:26 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package epam.Cleancode_Serialization;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+package epam.Cleancode_Serialization;
+
+public class Main {
+	
+		public static void main(String[] args) {
+			Calculationofinterest.Interest();
+			Codeconstruction.construction();
+		}
+	}
+
